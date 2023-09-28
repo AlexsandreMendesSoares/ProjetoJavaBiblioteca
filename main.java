f2579bfe42536feeaebe94a70d053a3b51d5e87c
alex <alexgtgames@gmail.com>
2023-09-28 20:27:30 -0300

    Commit main

---
+++ main.java
@@ -0,0 +1,12 @@
+package LocalBiblioteca.view;
+
+
+import LocalBiblioteca.DAO.Conexao;
+
+public class main {
+    public static void main(String[] args) {
+        Conexao conectar = new Conexao();
+        conectar.Conexao(); 
+
+    }
+}
